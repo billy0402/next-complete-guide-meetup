831c6dd29b767f9a2c91c090379b771ab7385721
Billy Huang <10646003@ntub.edu.tw>
2022-02-05 17:21:15 +0800

    feat(HomePage): How Pre-rendering Works & Which Problem We Face

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
+
 import type { NextPage } from 'next';
 
 import MeetupList from '@components/meetup/MeetupList';
 import { DUMMY_MEETUPS } from '@fixtures/meetups';
+import { Meetup } from '@models/meetup';
 
 const HomePage: NextPage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loadedMeetups, setLoadedMeetups] = useState<Meetup[]>([]);
+
+  useEffect(() => {
+    // send a http request and fetch data
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 };
 
 export default HomePage;
