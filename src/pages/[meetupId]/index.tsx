2bfb352090f74c69ce36e68b03f126f82d472377
Billy Huang <10646003@ntub.edu.tw>
2022-02-05 16:11:46 +0800

    feat(pages): Preparing the Project Pages

---
+++ src/pages/[meetupId]/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const MeetupDetailPage: NextPage = () => {
+  return <h1>MeetupDetail</h1>;
+};
+
+export default MeetupDetailPage;
---
+++ src/pages/new/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const NewMeetupPage: NextPage = () => {
+  return <h1>NewMeetup</h1>;
+};
+
+export default NewMeetupPage;
