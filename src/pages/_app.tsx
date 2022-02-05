af27f9f8b0d48ad53ed452d3348ad9ef32c06a40
Billy Huang <10646003@ntub.edu.tw>
2022-02-05 16:53:45 +0800

    feat(MyApp): The "_app.js" File & Layout Wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import Layout from '@components/layout/Layout';
 import '@styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Document</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 };
 
