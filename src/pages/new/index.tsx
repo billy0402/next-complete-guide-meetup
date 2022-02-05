8129cd2caf4338a2375153400f0d56bae831bcc7
Billy Huang <10646003@ntub.edu.tw>
2022-02-05 20:25:36 +0800

    feat(NewMeetupPage): Sending Http Requests To Our API Routes

--- src/pages/new/index.tsx
+++ src/pages/new/index.tsx
@@ -1,11 +1,23 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 import NewMeetupForm from '@components/meetup/NewMeetupForm';
 import { MeetupCreate } from '@models/meetup';
 
 const NewMeetupPage: NextPage = () => {
-  const addMeetupHandler = (meetup: MeetupCreate) => {
-    console.log(meetup);
+  const router = useRouter();
+
+  const addMeetupHandler = async (meetup: MeetupCreate) => {
+    const response = await fetch('/api/meetup', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(meetup),
+    });
+    const data = await response.json();
+    console.log(data);
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
